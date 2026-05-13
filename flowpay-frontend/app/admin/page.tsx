fda3540f3be3ad4f82dae3198fa9bac568c8c5fd
iyad <iyad@email.com>
2026-05-13 14:42:18 -0600

    Create admin dashboard

--- flowpay-frontend/app/admin/page.tsx
+++ flowpay-frontend/app/admin/page.tsx
@@ -1,18 +1,116 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import API_URL from "@/lib/api";
+
+interface User {
+  _id: string;
+  email: string;
+  balance: number;
+  role: string;
+  frozen: boolean;
+}
+
 export default function AdminPage() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    checkAdmin();
+    loadUsers();
+  }, []);
+
+  const checkAdmin = async () => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      window.location.href = "/login";
+      return;
+    }
+
+    try {
+      const res = await fetch(
+        `${API_URL}/me`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      const data = await res.json();
+
+      if (data.role !== "admin") {
+        alert("Access denied");
+        window.location.href =
+          "/dashboard";
+      }
+    } catch (err) {
+      alert("Server error");
+    }
+  };
+
+  const loadUsers = async () => {
+    try {
+      const res = await fetch(
+        `${API_URL}/users`
+      );
+
+      const data = await res.json();
+
+      setUsers(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div
       style={{
+        padding: 30,
         background: "#0f172a",
-        color: "white",
         minHeight: "100vh",
-        padding: 40,
+        color: "white",
       }}
     >
-      <h1>🛠 Admin Dashboard</h1>
+      <h1>
+        🛡 Admin Dashboard
+      </h1>
 
-      <p>Admin panel is working.</p>
+      <br />
+
+      {users.map((user) => (
+        <div
+          key={user._id}
+          style={{
+            background: "#111827",
+            padding: 20,
+            borderRadius: 15,
+            marginBottom: 15,
+          }}
+        >
+          <p>
+            <strong>Email:</strong>{" "}
+            {user.email}
+          </p>
+
+          <p>
+            <strong>Balance:</strong>{" "}
+            ${user.balance}
+          </p>
+
+          <p>
+            <strong>Role:</strong>{" "}
+            {user.role}
+          </p>
+
+          <p>
+            <strong>Frozen:</strong>{" "}
+            {user.frozen
+              ? "Yes"
+              : "No"}
+          </p>
+        </div>
+      ))}
     </div>
   );
 }
