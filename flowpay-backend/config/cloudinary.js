fc63d7b48f4370b0ee86015ce74be5bdffabaa60
Iyad <iydhsn73@gmail.com>
2026-09-13 14:50:12 +0300

    Fix Cloudinary environment variable names

--- flowpay-backend/config/cloudinary.js
+++ flowpay-backend/config/cloudinary.js
@@ -7,13 +7,13 @@ const cloudinary =
 
 cloudinary.config({
   cloud_name:
-    process.env.CLOUDINARY_CLOUD_NAME,
+    process.env.CLOUDINARY_NAME,
 
   api_key:
-    process.env.CLOUDINARY_API_KEY,
+    process.env.CLOUDINARY_KEY,
 
   api_secret:
-    process.env.CLOUDINARY_API_SECRET,
+    process.env.CLOUDINARY_SECRET,
 });
 
 // =========================
